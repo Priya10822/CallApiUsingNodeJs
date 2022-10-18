14a7fb7349d37e4c84cc089d014542db2b6b1663
Priya10822 <115445172+Priya10822@users.noreply.github.com>
2022-10-18 20:55:22 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const apiCall = require('./call')
+const apiCall = require("./call");
 
-const http = require('http')
+const http = require("http");
 
-http.createServer((req,res)=>{
-        apiCall.callApi(function(response){
-            console.log(response);
-            res.setHeader('Content-Type', 'text/plain');
-            res.write(JSON.stringify(response));
-            res.end();
-        });
-}).listen(8080);
+http
+  .createServer((req, res) => {
+    apiCall.callApi(function (response) {
+      console.log(response);
+      res.setHeader("Content-Type", "text/plain");
+      res.write(JSON.stringify(response));
+      res.end();
+    });
+  })
+  .listen(8080);
 
 console.log("Service running on 8080 port....");
